3b95ee22d38d3130f315dd7f049d0c1ca424e841
Michal Vrbata <vrbata@outlook.com>
2020-11-07 20:15:45 +0100

    Added gitconfig files

--- symlink.sh
+++ symlink.sh
@@ -5,3 +5,5 @@ DOTFILE_DIR=$HOME/.dotfiles
 ln -sv "$DOTFILE_DIR/zsh/.zshrc" "$HOME/.zshrc"
 ln -sv "$DOTFILE_DIR/zsh/.ackrc" "$HOME/.ackrc"
 ln -sv "$DOTFILE_DIR/zsh/.antigenrc" "$HOME/.antigenrc"
+ln -sv "$DOTFILE_DIR/git/.gitconfig" "$HOME/.gitconfig"
+ln -sv "$DOTFILE_DIR/git/.gitconfig-gli" "$HOME/.gitconfig-gli"
