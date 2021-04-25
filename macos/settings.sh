577e3fe4a78f093801d3bfc8756d93478e4c4e9e
Michal Vrbata <vrbata@outlook.com>
2021-04-25 11:39:06 +0200

    Add script to scale minimalization

--- macos/settings.sh
+++ macos/settings.sh
@@ -5,3 +5,6 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
 
 # Disable Font Smoothing
 defaults -currentHost write -g AppleFontSmoothing -int 0
+
+# Set the Scale Effect to Minimize Windows
+defaults write com.apple.dock mineffect -string scale
