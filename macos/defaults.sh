a54ba2fcddcd185997f54558a68151208a48cb20
Michal Vrbata <vrbata@outlook.com>
2021-04-25 13:17:03 +0200

    Update defaults

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -2,8 +2,9 @@
 
 ## macOS
 
-# To prevent the Mac workstation from creating .DS_Store files on network shares
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
+# Tvoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
 # Disable Font Smoothing
 defaults -currentHost write -g AppleFontSmoothing -int 0
@@ -24,6 +25,12 @@ defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
 # Disable the warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Show path bar
+defaults write com.apple.finder ShowPathbar -bool true
 
 ## iTerm
 
