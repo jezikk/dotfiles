2e27866b1f21392ab192ccb44ca42343b962f6e9
Michal Vrbata <vrbata@outlook.com>
2021-04-25 10:26:15 +0200

    Add font smoothing

--- macos/settings.sh
+++ macos/settings.sh
@@ -2,3 +2,6 @@
 
 # To prevent the Mac workstation from creating .DS_Store files on network shares
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
+
+# Disable Font Smoothing
+defaults -currentHost write -g AppleFontSmoothing -int 0
