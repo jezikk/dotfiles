12440a3b9dbfcdab9c945a72c09331b41fa13c6f
Michal Vrbata <vrbata@outlook.com>
2024-08-15 08:23:32 +0200

    Add git aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -20,6 +20,10 @@ alias vim="nvim"
 alias ls="eza --icons"
 alias la="eza -la --header --icons"
 
+alias dcd="docker compose down"
+alias dcu="docker compose up -d"
+alias dcub="docker compose up -d --build"
+
 alias gs="git status"
 alias gsb="git status --short --branch"
 alias ga="git add"
@@ -28,8 +32,7 @@ alias gcm="git commit --message"
 alias grm='git rm'
 alias grmc='git rm --cached'
 alias gp='git push -u'
-alias gll='git log --oneline --decorate --graph'
-alias glp="git log --pretty='%C(Yellow)%h  %C(reset)%ad (%C(Green)%cr%C(reset))%x09 %C(Cyan)%an: %C(reset)%s' --date=short -7"
+alias gl="git log --all --pretty='%C(Yellow)%h  %C(reset)%ad (%C(Green)%cr%C(reset))%x09 %C(Cyan)%an%C(reset)%x09 %C(red)%d %C(reset)%s' --date=short"
 alias gb="git checkout"
 alias gbc="git checkout -b"
 alias gbd="git branch -D"
@@ -38,6 +41,8 @@ alias gu="git pull"
 alias gur="git pull --rebase"
 alias gum="git pull --merge"
 alias gf="git fetch"
+alias gfs="git fetch; git status"
+alias gfl="git fetch; git --no-pager log --all --pretty='%C(Yellow)%h  %C(reset)%ad (%C(Green)%cr%C(reset))%x09 %C(Cyan)%an%C(reset)%x09 %C(red)%d %C(reset)%s' --date=short -20"
 
 alias cls="clear"
 alias ..="cd .."
